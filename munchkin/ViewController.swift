c4023f5d9eb783d02c385aff1b03629a9d35e692
Guilherme Siepmann <guilherme.sipmann@radixeng.com.br>
2018-04-30 07:37:48 -0300

    Initial Commit

---
+++ munchkin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  munchkin
+//
+//  Created by Guilherme Siepmann on 30/04/2018.
+//  Copyright © 2018 Guilherme Siepmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
